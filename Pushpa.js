a2dc2907f7f77f348a4c8fbb5c2a95b6adfa0961
SUMANPLAY <mrsumanplayz@gmail.com>
2024-12-07 20:46:18 +0530

    Pushpa.js

---
+++ Pushpa.js
@@ -0,0 +1,6 @@
+const videoPlayer = document.getElementById("videoPlayer");
+
+// Prevent right-click to block download options
+videoPlayer.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// Additional future customizations
